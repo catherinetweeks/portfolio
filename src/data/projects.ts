5ef16c27d78f42e412ab9b46b3ff40e6754be1f4
Catherine Weeks <catherinetweeks@gmail.com>
2025-08-21 11:40:33 -0400

    attempt to fix images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,7 @@
+const base = import.meta.env.BASE_URL;
 export const projects = [
     {
-        image: "../src/assets/projectphotos/bujo.png",
+        image: `${base}assets/projectphotos/bujo.png`,
         type: "Minimalist Bullet Journal Web App",
         title: "Digital Bullet Journal",
         description: "A sleek bullet journaling app that allows users to save notes and to-dos, as well as sort these entries by completion status, due date, and creation date.",
@@ -8,7 +9,7 @@ export const projects = [
         link: "https://github.com/catherinetweeks/minimalist-bujo",
     },
     {
-        image: "../src/assets/projectphotos/markdown.png",
+        image: `${base}assets/projectphotos/markdown.png`,
         type: "Custom Markdown Converter",
         title: "Rich Text to Markdown Editor",
         description: "A rich text editor made with React and Typescript that exports to markdown via copying the converted text to users' clipboard or downloading it as a .md file.",
@@ -16,7 +17,7 @@ export const projects = [
         link: "https://github.com/catherinetweeks/markdown-editor",
     },
     {
-        image: "../src/assets/projectphotos/lightning.png",
+        image: `${base}assets/projectphotos/lightning.png`,
         type: "Pygame Pixel Game with Custom Artwork",
         title: "Lightning in a Bottle",
         description: "A cute, arcade-style 2D pixel game made in Python using Pygame that utilizes object oriented programming and event-driven architecture",
